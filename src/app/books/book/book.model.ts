56a6885cee0be0e2179215bf841d0b762530ee70
Simonas B <Simonas.birbalas@gmail.com>
2021-11-23 18:08:17 +0200

    first commit

---
+++ src/app/books/book/book.model.ts
@@ -0,0 +1,18 @@
+import {Author} from "./author.model";
+import {Subjects} from "../../category/subjects.type";
+import {Languages} from "./languages.type";
+import {Authors} from "./authors.type";
+import {Bookshelves} from "./bookshelves.type";
+import {Formats} from "./formats.type";
+
+export class Book {
+  id: number | null = null;
+  authors: Authors = [];
+  bookshelves: Bookshelves = [];
+  download_count: number = 0;
+  formats: { [key: string]: string } = {};
+  languages: Languages = [];
+  media_type: string = "";
+  subjects: Subjects = [];
+  title: string = "";
+}
